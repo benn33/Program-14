62913654f876a196e33b6673a762bbb401c0ec69
benn33 <rodebe@mnstate.edu>
2016-04-27 03:18:12 -0500

    Intersection / doc
    I added the Intersection method using __and__, and then added documentation to all of the methods.

--- P14Test.py
+++ P14Test.py
@@ -1,4 +1,8 @@
 #Initial Test File
 from modCustomSet import *
-set01 = CustomSet([10,20,30,40,20,30,40,10,34])
+set01 = CustomSet([10,20,40,50,55])
+set02 = CustomSet([10,30,40,55,90])
+set03 = set01 & set02
+
 print(set01)
+print(set03)
